41634feb8452eebc50cb8cd1fb5b14699b37ec08
Gaeph-Phico de Freud JN PHILIPPE <107807964+Gaeph@users.noreply.github.com>
2025-11-03 13:49:57 -0500

    Implement user and review resources in app.py
    Added user and review resources to handle user and review data.

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 from flask import request
 from flask_restful import Resource
 from config import app, db, api
-from models import Movie
+from models import Movie, User, Review, UserMovie
 
+# ----------------- MOVIES -----------------
 class MoviesResource(Resource):
     def get(self):
         movies = [m.to_dict() for m in Movie.query.all()]
@@ -20,7 +21,6 @@ class MoviesResource(Resource):
         db.session.add(movie)
         db.session.commit()
         return movie.to_dict(), 201
-
 
 class MovieResource(Resource):
     def get(self, movie_id):
@@ -42,9 +42,76 @@ class MovieResource(Resource):
         db.session.commit()
         return {}, 204
 
+# ----------------- USERS -----------------
+class UsersResource(Resource):
+    def get(self):
+        users = [u.to_dict() for u in User.query.all()]
+        return {"users": users}, 200
 
+    def post(self):
+        data = request.get_json() or {}
+        user = User(
+            username=data.get("username"),
+            email=data.get("email")
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user.to_dict(), 201
+
+class UserResource(Resource):
+    def get(self, user_id):
+        user = User.query.get_or_404(user_id)
+        return user.to_dict(), 200
+
+# ----------------- REVIEWS -----------------
+class ReviewsResource(Resource):
+    def get(self):
+        reviews = [r.to_dict() for r in Review.query.all()]
+        return {"reviews": reviews}, 200
+
+    def post(self):
+        data = request.get_json() or {}
+        review = Review(
+            content=data.get("content"),
+            rating=data.get("rating"),
+            user_id=data.get("user_id"),
+            movie_id=data.get("movie_id")
+        )
+        db.session.add(review)
+        db.session.commit()
+        return review.to_dict(), 201
+
+class ReviewResource(Resource):
+    def get(self, review_id):
+        review = Review.query.get_or_404(review_id)
+        return review.to_dict(), 200
+
+# ----------------- USER-MOVIE (Many-to-Many) -----------------
+class UserMovieResource(Resource):
+    def post(self):
+        data = request.get_json() or {}
+        assoc = UserMovie(
+            user_id=data.get("user_id"),
+            movie_id=data.get("movie_id"),
+            user_rating=data.get("user_rating"),
+            favorite=data.get("favorite", False)
+        )
+        db.session.add(assoc)
+        db.session.commit()
+        return assoc.to_dict(), 201
+
+# ----------------- ROUTES -----------------
 api.add_resource(MoviesResource, '/movies')
 api.add_resource(MovieResource, '/movies/<int:movie_id>')
 
+api.add_resource(UsersResource, '/users')
+api.add_resource(UserResource, '/users/<int:user_id>')
+
+api.add_resource(ReviewsResource, '/reviews')
+api.add_resource(ReviewResource, '/reviews/<int:review_id>')
+
+api.add_resource(UserMovieResource, '/user_movies')
+
+# ----------------- RUN SERVER -----------------
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
